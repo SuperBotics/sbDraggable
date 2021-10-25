2628aebe3d6565b406a84643adeb627c5d0c9464
Sudarshan Anbazhagan <a.sudarshann@gmail.com>
2021-10-26 01:44:55 +0530

    Update sbDraggable.js

--- src/sbDraggable.js
+++ src/sbDraggable.js
@@ -4,70 +4,80 @@ class sbDraggable {
         this.querySelector = querySelector;
         this.options = options;
 
+        this.draggables = [];
+        var instance = this;
+
         this.draggables = document.querySelectorAll(querySelector);
-        [].forEach.call(this.draggables, this.addDnDHandlers);
+        [].forEach.call(this.draggables, function(element) {
+            element.setAttribute("draggable", "true")
+            instance.addDnDHandlers(element, instance);
+        });
 
         return this;
     }
 
-    handleDragStart(e) {
+    handleDragStart(e, instance) {
         // Target (this) element is the source node.
-        this.dragSrcEl = this;
+
+        this.dragSrcEl = instance;
+
 
         e.dataTransfer.effectAllowed = 'move';
-        e.dataTransfer.setData('text/html', this.outerHTML);
+        e.dataTransfer.setData('text/html', instance.outerHTML);
 
-        this.classList.add('sb-dragging-start');
+        instance.classList.add('sb-dragging-start');
     }
-    handleDragOver(e) {
+    handleDragOver(e, instance) {
         if (e.preventDefault) {
             e.preventDefault(); // Necessary. Allows us to drop.
         }
-        this.classList.add('over');
+        instance.classList.add('sb-dragging-over');
 
         e.dataTransfer.dropEffect = 'move'; // See the section on the DataTransfer object.
 
         return false;
     }
-    handleDragEnter(e) {
+    handleDragEnter(e, instance) {
         // this / e.target is the current hover target.
     }
-    handleDragLeave(e) {
-        this.classList.remove('sb-dragging-done'); // this / e.target is previous target element.
+    handleDragLeave(e, instance) {
+        instance.classList.remove('sb-dragging-over'); // this / e.target is previous target element.
     }
-    handleDrop(e) {
+    handleDrop(e, instance) {
         // this/e.target is current target element.
 
         if (e.stopPropagation) {
             e.stopPropagation(); // Stops some browsers from redirecting.
         }
 
+        if (this.dragSrcEl === null) {
+            return;
+        }
         // Don't do anything if dropping the same column we're dragging.
-        if (this.dragSrcEl != this) {
-            this.parentNode.removeChild(this.dragSrcEl);
-            var dropHTML = e.dataTransfer.getData('text/html');
-            this.insertAdjacentHTML('beforebegin', dropHTML);
-            var dropElement = this.previousSibling;
-            addDnDHandlers(dropElement);
+        if (this.dragSrcEl != instance) {
+            instance.parentNode.removeChild(this.dragSrcEl);
+            //var dropHTML = e.dataTransfer.getData('text/html');
+            instance.insertAdjacentElement('beforebegin', this.dragSrcEl);
+            var dropElement = instance.previousSibling;
+            this.addDnDHandlers(dropElement, this);
 
         }
-        this.classList.remove('sb-dragging-start');
-        this.classList.remove('sb-dragging-done');
+        instance.classList.remove('sb-dragging-start');
+        instance.classList.remove('sb-dragging-over');
         return false;
     }
-    handleDragEnd(e) {
-        this.classList.remove('sb-dragging-start');
-        this.classList.remove('sb-dragging-done');
+    handleDragEnd(e, instance) {
+        instance.classList.remove('sb-dragging-start');
+        instance.classList.remove('sb-dragging-over');
     }
-    addDnDHandlers(elem) {
-        elem.addEventListener('dragstart', this.handleDragStart, false);
-        elem.addEventListener('dragenter', this.handleDragEnter, false)
-        elem.addEventListener('dragover', this.handleDragOver, false);
-        elem.addEventListener('dragleave', this.handleDragLeave, false);
-        elem.addEventListener('drop', this.handleDrop, false);
-        elem.addEventListener('dragend', this.handleDragEnd, false);
+    addDnDHandlers(element, instance) {
+        element.addEventListener('dragstart', function(event) { instance.handleDragStart(event, this) }, false);
+        element.addEventListener('dragenter', function(event) { instance.handleDragEnter(event, this) }, false)
+        element.addEventListener('dragover', function(event) { instance.handleDragOver(event, this) }, false);
+        element.addEventListener('dragleave', function(event) { instance.handleDragLeave(event, this) }, false);
+        element.addEventListener('drop', function(event) { instance.handleDrop(event, this) }, false);
+        element.addEventListener('dragend', function(event) { instance.handleDragEnd(event, this) }, false);
     }
 }
 
-window.sbDraggable = sbDraggable;
-//let sbDraggable = new sbsbDraggable("#columns .column", {});
+//let sbDraggable = new sbDraggable("#columns .column", {});
