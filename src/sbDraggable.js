fe815661140b6c839a5cba6c7134a702db6973ce
Sudarshan Anbazhagan <a.sudarshann@gmail.com>
2021-10-28 00:31:04 +0530

    Update sbDraggable.js

--- src/sbDraggable.js
+++ src/sbDraggable.js
@@ -2,7 +2,18 @@ class sbDraggable {
     constructor(querySelector, options = {}) {
         this.dragSrcEl = null;
         this.querySelector = querySelector;
-        this.options = options;
+
+        let defaultOptions = {
+            afterHandleDragStart: function(element, elementInstance, sbDraggable) { return; },
+            afterHandleDragOver: function(element, elementInstance,sbDraggable) { return; },
+            afterHandleDragEnter: function(element, elementInstance, sbDraggable) { return; },
+            afterHandleDragLeave: function(element, elementInstance, sbDraggable) { return; },
+            afterHandleDrop: function(element, elementInstance, sbDraggable) { return; },
+            afterHandleDragEnd: function(element, elementInstance, sbDraggable) { return; }
+        };
+
+        this.options = Object.assign({}, defaultOptions, options);
+
 
         this.draggables = [];
         var instance = this;
@@ -26,6 +37,9 @@ class sbDraggable {
         e.dataTransfer.setData('text/html', instance.outerHTML);
 
         instance.classList.add('sb-dragging-start');
+
+        let callback = this.options.afterHandleDragStart;
+        callback(e, instance, this);
     }
     handleDragOver(e, instance) {
         if (e.preventDefault) {
@@ -35,13 +49,22 @@ class sbDraggable {
 
         e.dataTransfer.dropEffect = 'move'; // See the section on the DataTransfer object.
 
+        let callback = this.options.afterHandleDragOver;
+        callback(e, instance, this);
+
         return false;
     }
     handleDragEnter(e, instance) {
         // this / e.target is the current hover target.
+
+        let callback = this.options.afterHandleDragEnter;
+        callback(e, instance, this);
     }
     handleDragLeave(e, instance) {
         instance.classList.remove('sb-dragging-over'); // this / e.target is previous target element.
+
+        let callback = this.options.afterHandleDragLeave;
+        callback(e, instance, this);
     }
     handleDrop(e, instance) {
         // this/e.target is current target element.
@@ -64,11 +87,18 @@ class sbDraggable {
         }
         instance.classList.remove('sb-dragging-start');
         instance.classList.remove('sb-dragging-over');
+
+        let callback = this.options.afterHandleDrop;
+        callback(e, instance, this);
+
         return false;
     }
     handleDragEnd(e, instance) {
         instance.classList.remove('sb-dragging-start');
         instance.classList.remove('sb-dragging-over');
+
+        let callback = this.options.afterHandleDragEnd;
+        callback(e, instance, this);
     }
     addDnDHandlers(element, instance) {
         element.addEventListener('dragstart', function(event) { instance.handleDragStart(event, this) }, false);
@@ -80,4 +110,5 @@ class sbDraggable {
     }
 }
 
+window.sbDraggable = sbDraggable;
 //let sbDraggable = new sbDraggable("#columns .column", {});
